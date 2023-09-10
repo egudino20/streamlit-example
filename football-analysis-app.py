e7b9eb503b86edf6ac1c29cd773e47d2f72c4726
egudino20 <53064236+egudino20@users.noreply.github.com>
2023-09-10 14:02:54 -0400

    Update football-analysis-app.py

--- football-analysis-app.py
+++ football-analysis-app.py
@@ -36,7 +36,7 @@ def main():
     }
     league_folder = league_to_comp[country]
 
-    main_folder = r"analysis-tools"
+    main_folder = r"streamlit-example"
     filename = "matches_data.json"
     #filepath = os.path.join(main_folder, "Data", league_folder, year, "match-data", filename)
     filepath = "https://storage.cloud.google.com/matches-data/matches_data.json"
