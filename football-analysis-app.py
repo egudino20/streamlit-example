c6f4177967a95211748b384c685f574619ce395a
egudino20 <53064236+egudino20@users.noreply.github.com>
2023-10-07 02:51:15 +0000

    Upload files

--- football-analysis-app.py
+++ football-analysis-app.py
@@ -650,7 +650,7 @@ def main():
     
     elif option == 'Match Week Zone Control':
         
-        match_weeks = [1, 2, 3, 4, 5, 6]
+        match_weeks = [1, 2, 3, 4, 5, 6, 7]
 
         if country == 'England':
             premier_league_teams = ['Arsenal', 'Aston Villa', 'Brentford', 'Brighton & Hove Albion', 
