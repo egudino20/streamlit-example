e71fe302b3ade2b475a705a6a6c074e34b0d0dcf
egudino20 <53064236+egudino20@users.noreply.github.com>
2023-10-11 01:10:26 +0000

    update load data

--- load_raw_data.py
+++ load_raw_data.py
@@ -57,3 +57,89 @@ import datetime
 from PIL import Image
 import requests
 from io import BytesIO
+
+# Load Data
+import os
+import json
+
+# pick season we are loading data for
+league_folder = 'premier-league'
+season = 2024
+
+def load_matches_data(league_folder, season):
+
+    # Define the folder where your JSON files are located
+    folder_path = os.path.join("Data", f"{league_folder}", f"{season}", "match-data")
+
+    # Initialize an empty list to store the concatenated data
+    matches_data = []
+
+    # Iterate through files in the folder
+    for file_name in os.listdir(folder_path):
+        if "matches_data-" in file_name and file_name.endswith(".json"):
+            # Construct the full path to the JSON file
+            file_path = os.path.join(folder_path, file_name)
+
+            # Open and load the JSON data
+            with open(file_path, "r") as f:
+                data = json.load(f)
+
+            # Extend the concatenated_data list with the data from the current file
+            matches_data.extend(data)
+
+    # Define the output file path for the concatenated data
+    # output_file_path = os.path.join("Data", "premier-league", "2024", "match-data", "matches_data.json")
+
+    return matches_data
+
+matches_data = load_matches_data(league_folder, season)
+
+def load_events_df(matches_data):
+
+    # league identifiers
+    league = f"{matches_data[0]['region']} {matches_data[0]['league']} {matches_data[0]['season']}"
+
+    season = matches_data[0]['season']
+
+    # file path and title positioning conditions
+    if matches_data[0]['region'] == 'Argentina':
+        comp = 'liga-profesional'
+    elif matches_data[0]['region'] == 'Spain':
+        comp = 'la-liga'
+    elif matches_data[0]['region'] == 'England':
+        comp = 'premier-league'
+    elif matches_data[0]['region'] == 'France':
+        comp = 'ligue-1'
+    elif matches_data[0]['region'] == 'Germany':
+        comp = 'bundesliga'
+    else:
+        comp = 'serie-a'
+
+    # Define the directory and pattern to search for files
+    directory = f'Data/{comp}/{season[5:]}/raw-season-data/'
+    pattern = f'{comp}-*.csv'
+
+    # Use glob to find all matching files
+    file_list = glob.glob(directory + pattern)
+
+    # Initialize an empty list to store DataFrames
+    dfs = []
+
+    # Loop through the list of matching files and read them into DataFrames
+    for file in file_list:
+        df = pd.read_csv(file, low_memory=False)
+        # Drop the 'Unnamed: 0' column if it exists
+        if 'Unnamed: 0' in df.columns:
+            df.drop('Unnamed: 0', axis=1, inplace=True)
+        dfs.append(df)
+
+    # Concatenate all DataFrames into a single DataFrame
+    events_df = pd.concat(dfs, ignore_index=True)
+
+    return events_df
+
+    # Now you have events_df containing data from all matching files
+
+
+# export and process match_data through current date; save process and exported dfs to dfs
+dfs = process_and_export_match_data(events_df, matches_data, comp, season)
